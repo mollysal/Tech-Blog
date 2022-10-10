356e4801b5310d80fca6ca6264b5a2fd43bad40d
Molly Salmonsen <mollysalmonsen@Mollys-MacBook-Pro-2.local>
2022-10-09 20:50:28 -0400

    Adding Comments to homeRoutes & working on User Routes

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const { Post, User, Comment } = require('../models');
+const withAuth = require('../utils/auth');
+
+// Get /api/users
+
+// Get /api/users/#
+
+// Post /api/users
+
+// Post /api/login
+
+// Post /api/logout
+
+// Put /api/users/# (Updating Post)
+
+// Delete /api/users/# (Deleting Post)
+
+module.exports = router; 
